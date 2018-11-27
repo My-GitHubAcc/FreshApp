9290a3bfd46bcf7fcae4e5386f7b3092c12d16ae
Adwin <dev_AccLive@users.noreply.github.com>
2018-11-27 15:40:52 +0000

    Project Main Activity

---
+++ app/src/main/java/com/nibrasco/freshapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.nibrasco.freshapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn, btnSignUp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignIn = (Button)findViewById(R.id.btnSignUp);
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+    }
+}
