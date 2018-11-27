ddf0bfa24a9c8d61667fa26cca01e1d670cc0870
Adwin <dev_AccLive@users.noreply.github.com>
2018-11-27 17:07:27 +0000

    Sign In & Sign Up: Connection to the FirebaseDb Established & functionnality stable

--- app/src/main/java/com/nibrasco/freshapp/SignIn.java
+++ app/src/main/java/com/nibrasco/freshapp/SignIn.java
@@ -1,11 +1,15 @@
 package com.nibrasco.freshapp;
 
 import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import com.google.firebase.database.*;
+import com.nibrasco.freshapp.Model.User;
 
 public class SignIn extends AppCompatActivity {
 
@@ -20,12 +24,41 @@ public class SignIn extends AppCompatActivity {
 
         btnSignIn = (Button)findViewById(R.id.btnSignIn);
 
+        final FirebaseDatabase db = FirebaseDatabase.getInstance();
+        final DatabaseReference tblUser = db.getReference("User");
+
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final ProgressDialog mDialog =new ProgressDialog(SignIn.this);
                 mDialog.setMessage("Authenticating...");
                 mDialog.show();
+
+                tblUser.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.child(edtPhone.getText().toString()).exists()) {
+                            mDialog.dismiss();
+
+                            User user = dataSnapshot.child(edtPhone.getText().toString()).getValue(User.class);
+                            if (user.getPassword().equals(edtPwd.getText().toString())) {
+                                Toast.makeText(SignIn.this, "Sign In Successfull!", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(SignIn.this, "Sign In Failed!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else
+                        {
+                            mDialog.dismiss();
+                            Toast.makeText(SignIn.this, "User Inexistant, please create and Account!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
             }
         });
     }
